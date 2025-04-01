ef0f6535e4f6833ee210e102031a9a2405935c85
GuedesDev01 <guedesbruno1011@gmail.com>
2025-04-01 11:04:04 -0300

    first commit

---
+++ src/componentes/Botao.jsx
@@ -0,0 +1,13 @@
+const Botao = () => {
+
+    return(
+         <>
+        <input type="button" 
+          value="clicar" 
+          />
+        </>
+    )
+    
+    }
+    
+    export default Botao
---
+++ src/componentes/Etiqueta.jsx
@@ -0,0 +1,23 @@
+import Botao from "./componentes/Botao"
+import Caixa from "./componentes/Caixa"
+import Etiqueta from "./componentes/Etiqueta"
+
+const Etiqueta = () => {
+
+return(
+     <>
+      Aula de Componentes
+
+      <Etiqueta texto="texto envidao" />
+      <Etiqueta />
+      <Etiqueta />
+      <Caixa />
+      <Caixa />
+      <Caixa />
+      <Botao />
+    </>
+)
+
+}
+
+export default Etiqueta
